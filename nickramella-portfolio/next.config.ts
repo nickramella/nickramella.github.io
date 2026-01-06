c9fad2b8aac9634905a40d4f33578ff00e462a15
Nicholas Ramella <ramellanick1@gmail.com>
2026-01-05 18:38:55 -0800

    Github Workflows

--- nickramella-portfolio/next.config.ts
+++ nickramella-portfolio/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // Enables static HTML export
   // Optionally, set the basePath if deploying to a project page (e.g., username.github.io/repo-name)
-  basePath: '/nickramella-portfolio',
+  basePath: '/nickramella-portfolio/',
+  reactStrictMode: true,
   // assetPrefix: '/nickramella.github.io/',
 };
 
