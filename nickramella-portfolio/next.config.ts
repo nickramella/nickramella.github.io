1a0ffd5029af8364d3038b24b6bd2b9a0fcfd8d9
Nicholas Ramella <ramellanick1@gmail.com>
2026-01-05 18:41:47 -0800

    assetPrefix

--- nickramella-portfolio/next.config.ts
+++ nickramella-portfolio/next.config.ts
@@ -2,10 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static HTML export
-  // Optionally, set the basePath if deploying to a project page (e.g., username.github.io/repo-name)
   basePath: '/nickramella-portfolio/',
   reactStrictMode: true,
-  // assetPrefix: '/nickramella.github.io/',
+  assetPrefix: '/nickramella.github.io/',
 };
 
 export default nextConfig;
