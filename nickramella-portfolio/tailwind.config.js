b0bf1201d52fd4725788aafe43d1bcd257cdab46
Nicholas Ramella <ramellanick1@gmail.com>
2026-01-05 19:11:00 -0800

    Tailwind config

---
+++ nickramella-portfolio/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue,html}", // <-- Make sure this covers all your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
