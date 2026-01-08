0bb7a6d5afd8533fbc53c0111ecbaf744ff1d8f6
Nicholas Ramella <ramellanick1@gmail.com>
2026-01-08 13:31:20 -0800

    Update Home Page description

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           <div className={`justify-end ${!isSmallScreen && "w-[50%]"}`}>
             <h1 className="text-center font-bold py-3">Full Stack Development | Data Analysis | OSINT</h1>
             <p className="fluid-text pb-5">
-              <i className="fa fa-angle-right"/>Software engineer with a background in statistics and data analysis, specializing in full-stack development and cloud-based enterprise platforms. Experience modernizing legacy applications and business intelligence dashboards into scalable, secure, and ADA- and WCAG-compliant solutions. Experience spans front-end frameworks, and backend services while working in an Agile environment.
+              <i className="fa fa-angle-right"/>Software engineer with a background in statistics and data analysis, specializing in full-stack development and cloud-based enterprise platforms. Experience modernizing legacy applications and business intelligence dashboards into scalable, secure, ADA- and WCAG-compliant solutions. Experience spans front-end frameworks, and backend services while working in an Agile environment.
             </p>
           </div>
         </div>
